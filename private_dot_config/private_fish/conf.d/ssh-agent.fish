1e31f01f59bebd92374cab1f0a613d7bab574203
Nicolas Karolak <nicolas@karolak.fr>
2022-07-19 09:57:08 +0200

    fish: set secretive as ssh agent

--- private_dot_config/private_fish/conf.d/ssh-agent.fish
+++ private_dot_config/private_fish/conf.d/ssh-agent.fish
@@ -2,7 +2,9 @@
 
 if status is-interactive
 and not set -q SSH_AUTH_SOCK
-    if pgrep ssh-agent | string collect > /dev/null
+    if test -e $HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+        set -Ux SSH_AUTH_SOCK $HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+    else if pgrep ssh-agent | string collect > /dev/null
         set -Ux SSH_AUTH_SOCK (find "/tmp" -maxdepth 2 -user $USER -type s -name agent.\* 2> /dev/null)
     else
         eval (ssh-agent -c)
