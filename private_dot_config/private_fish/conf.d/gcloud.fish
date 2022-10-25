8490d0c00e7249f37687ababe8965a41100eceaf
Nicolas Karolak <nicolas@karolak.fr>
2022-10-25 20:10:10 +0200

    fish: source gcloud path config script

--- private_dot_config/private_fish/conf.d/gcloud.fish
+++ private_dot_config/private_fish/conf.d/gcloud.fish
@@ -1,3 +1,6 @@
 # gcloud settings
 
+if test -f "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+	status is-interactive; and source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+end
 set -gx USE_GKE_GCLOUD_AUTH_PLUGIN True
